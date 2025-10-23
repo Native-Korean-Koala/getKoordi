46d6d383ec86e9174af068bebd65316e2ef96256
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-10-24 04:08:03 +0900

    init

---
+++ src/main/java/nativekoreankoala/demo/GetkoordiApplication.java
@@ -0,0 +1,13 @@
+package nativekoreankoala.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetkoordiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetkoordiApplication.class, args);
+	}
+
+}
